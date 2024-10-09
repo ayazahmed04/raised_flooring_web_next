4609ecb6ad9b9ea2589d43d17e8c429840d6da75
yazi <forgithub44@gmail.com>
2024-10-09 12:28:03 +0400

    completed company Website

---
+++ README.md
@@ -0,0 +1,82 @@
+
+# Modern UI/UX Website
+
+This repository contains a modern UI/UX website developed using **React.js** and **Tailwind CSS**. The project exemplifies modern UI/UX principles with its sleek design, seamless animations, and overall user experience, setting a high standard for future applications or websites.
+
+If you're getting started and need assistance or encounter any bugs, join our active Discord community with over **27k+ members**, where people help each other out.
+
+## ⚙️ Tech Stack
+
+- **Vite**
+- **React.js**
+- **Tailwind CSS**
+
+## 🔋 Features
+
+- **Beautiful Sections**: Includes hero, services, features, how to use, roadmap, pricing, footer, and header.
+- **Parallax Animations**: Engaging effects triggered by mouse movement and scrolling.
+- **Complex UI Geometry**: Utilizes Tailwind CSS for intricate shapes like circular feature displays, grid lines, and side lines.
+- **Latest UI Trends**: Incorporates modern design elements such as bento grids.
+- **Cool Gradients**: Enhances visuals with stylish gradients using Tailwind CSS for cards, buttons, etc.
+- **Responsive**: Ensures seamless functionality and aesthetics across all devices.
+- And many more, including code architecture and reusability.
+
+## 🤸 Quick Start
+
+Follow these steps to set up the project locally on your machine.
+
+### Prerequisites
+
+Ensure you have the following installed:
+
+- **Git**
+- **Node.js**
+- **npm** (Node Package Manager)
+
+### Cloning the Repository
+
+```bash
+```
+
+### Installation
+
+Install the project dependencies:
+
+```bash
+npm install
+```
+
+### Running the Project
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:5173](http://localhost:5173) in your browser to view the project.
+
+## 🕸️ Snippets
+
+- `.vscode/settings.json`
+- `tailwind.config.js`
+- `index.css`
+- `constants/index.js`
+- `components/Section.jsx`
+- `components/Roadmap.jsx`
+
+## 🔗 Links
+
+- [Assets](#)
+- [Design](#)
+- [Absolute Relative Positioning](#)
+- [Live Website](https://topraisedflooring.com)
+
+## Contributing
+
+Contributions are welcome! Please open an issue or submit a pull request for any improvements or bug fixes.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for details.
+
+## Thanks from AYAZ AHMED
+
---
+++ src/app/resources/content.js
@@ -0,0 +1,300 @@
+import { InlineCode } from '@/once-ui/components'
+
+const person = {
+   firstName: 'Top Raised',
+   lastName: 'Flooring',
+   get name() {
+      return `${this.firstName} ${this.lastName}`
+   },
+   role: 'Raised Static Floor',
+   avatar: '/images/avatar.jpg',
+   location: 'Asia/Dubai', // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
+   languages: ['English', 'Chinese'], // optional: Leave the array empty if you don't want to display languages
+}
+
+const newsletter = {
+   display: true,
+   title: <>Subscribe to {person.firstName}'s Newsletter</>,
+   description: (
+      <>
+         I occasionally write about design, technology, and share thoughts on
+         the intersection of creativity and engineering.
+      </>
+   ),
+}
+
+const social = [
+   // Links are automatically displayed.
+   // Import new icons in /once-ui/icons.ts
+   {
+      name: 'GitHub',
+      icon: 'github',
+      link: 'https://github.com/once-ui-system/nextjs-starter',
+   },
+   {
+      name: 'LinkedIn',
+      icon: 'linkedin',
+      link: 'https://www.linkedin.com/company/once-ui/',
+   },
+   {
+      name: 'X',
+      icon: 'x',
+      link: '',
+   },
+   {
+      name: 'Email',
+      icon: 'email',
+      link: 'mailto:adraisedflooring@gmail.com',
+   },
+]
+
+const home = {
+   label: 'Home',
+   title: `${person.name}'s Portfolio`,
+   description: `Portfolio website showcasing my work as a ${person.role}`,
+   headline: <>Design engineer and builder</>,
+   subline: (
+      <>
+         We want to be number <InlineCode>ONE</InlineCode>, Provider of
+         <br /> Raised Flooring Solution in Asia based in Dubai, China and
+         Singaport.
+         <br /> We're expert in Global Export All over the world.
+      </>
+   ),
+}
+
+const about = {
+   label: 'About',
+   title: 'About Us',
+   description: `Meet ${person.name}, ${person.role} from ${person.location}`,
+   tableOfContent: {
+      display: true,
+      subItems: true,
+   },
+   avatar: {
+      display: true,
+   },
+   calendar: {
+      display: true,
+      link: 'https://cal.com',
+   },
+   intro: {
+      display: true,
+      title: 'Introduction',
+      description: (
+         <>
+            Raised Flooring Atop FLOOR MAKING CO., LTD. is a integrated
+            enterprise concern with designing, researching, manufacturing,
+            servicing. From early 1980s, She has committed herself to develop,
+            produce and market raised access floor systems. Kehua adhere to
+            integrity, top quality, strict management, innovation and
+            development in the past twenty years; Our pragmatic and innovative
+            attitude win our customers’ praise so that the "KEHUA" became the
+            anti-static raised floor industry trustworthy brand suppliers.
+         </>
+      ),
+   },
+   work: {
+      display: true, // set to false to hide this section
+      title: 'Our Projects',
+      experiences: [
+         {
+            company: 'China telecom cloud Part',
+            timeframe: '2002',
+            role: 'Anti StaticRaised Floor',
+            achievements: [
+               <>
+                  We provide them with the need of raised flooring atop need in
+                  the best way possible so they can make fortune.
+               </>,
+               <>
+                  Best Way to make the Atop Raised FLooring in the Dubai and
+                  gulf Content
+               </>,
+            ],
+            images: [
+               // optional: leave the array empty if you don't want to display images
+               {
+                  src: '/images/projects/project-01/cover-01.jpg',
+                  alt: 'Raised Flooring  Atop ',
+                  width: 16,
+                  height: 9,
+               },
+            ],
+         },
+         {
+            company: 'China Civil Activation',
+            timeframe: '2018 - 2022',
+            role: 'Lead Flooring Manager ',
+            achievements: [
+               <>
+                  China civil activation park need our services for their
+                  project in raised flooring solution in dubai{' '}
+               </>,
+               <>
+                  Atop Raised Flooring make their look in the project of their
+                  studies and help them gain insight about project
+               </>,
+            ],
+            images: [],
+         },
+      ],
+   },
+   studies: {
+      display: true, // set to false to hide this section
+      title: 'Studies',
+      institutions: [
+         {
+            name: 'University of Jakarta',
+            description: <>Studied software engineering.</>,
+         },
+         {
+            name: 'Build the Future',
+            description: <>Studied online marketing and personal branding.</>,
+         },
+      ],
+   },
+   technical: {
+      display: true, // set to false to hide this section
+      title: 'Technical skills',
+      skills: [
+         {
+            title: 'Raised Floor ',
+            description: (
+               <>
+                  Ability to provide comapnies with best Raised Floor solution
+                  they need for their environment.
+               </>
+            ),
+            images: [
+               {
+                  src: '/images/projects/project-01/cover-02.jpg',
+                  alt: 'Atop Riased Flooring',
+                  width: 16,
+                  height: 9,
+               },
+               {
+                  src: '/images/projects/project-01/cover-03.jpg',
+                  alt: 'Project image',
+                  width: 16,
+                  height: 9,
+               },
+            ],
+         },
+         {
+            title: 'Static Raised Flooring',
+            description: (
+               <>
+                  Building Anti Static Raised Flooring Solution for our customer
+                  Happines
+               </>
+            ),
+            images: [
+               {
+                  src: '/images/projects/project-01/cover-04.jpg',
+                  alt: 'Project image',
+                  width: 16,
+                  height: 9,
+               },
+            ],
+         },
+      ],
+   },
+}
+
+const blog = {
+   label: 'Blog',
+   title: 'Writing about Raised Flooring and tech...',
+   description: `Read what ${person.name} has been up to recently`,
+   // Create new blog posts by adding a new .mdx file to app/blog/posts
+   // All posts will be listed on the /blog route
+}
+
+const work = {
+   label: 'Work',
+   title: 'My projects',
+   description: `Design and dev projects by ${person.name}`,
+   // Create new project pages by adding a new .mdx file to app/blog/posts
+   // All projects will be listed on the /home and /work routes
+}
+
+const gallery = {
+   label: 'Gallery',
+   title: 'My photo gallery of Raised Flooring Atop',
+   description: `A photo collection by ${person.name}`,
+   // Images from https://pexels.com
+   images: [
+      {
+         src: '/images/gallery/img-01.jpg',
+         alt: 'image',
+         orientation: 'vertical',
+      },
+      {
+         src: '/images/gallery/img-02.jpg',
+         alt: 'image',
+         orientation: 'horizontal',
+      },
+      {
+         src: '/images/gallery/img-03.jpg',
+         alt: 'image',
+         orientation: 'vertical',
+      },
+      {
+         src: '/images/gallery/img-04.jpg',
+         alt: 'image',
+         orientation: 'horizontal',
+      },
+      {
+         src: '/images/gallery/img-05.jpg',
+         alt: 'image',
+         orientation: 'horizontal',
+      },
+      {
+         src: '/images/gallery/img-06.jpg',
+         alt: 'image',
+         orientation: 'vertical',
+      },
+      {
+         src: '/images/gallery/img-07.jpg',
+         alt: 'image',
+         orientation: 'horizontal',
+      },
+      {
+         src: '/images/gallery/img-08.jpg',
+         alt: 'image',
+         orientation: 'vertical',
+      },
+      {
+         src: '/images/gallery/img-09.jpg',
+         alt: 'image',
+         orientation: 'horizontal',
+      },
+      {
+         src: '/images/gallery/img-10.jpg',
+         alt: 'image',
+         orientation: 'horizontal',
+      },
+      {
+         src: '/images/gallery/img-11.jpg',
+         alt: 'image',
+         orientation: 'vertical',
+      },
+      {
+         src: '/images/gallery/img-12.jpg',
+         alt: 'image',
+         orientation: 'horizontal',
+      },
+      {
+         src: '/images/gallery/img-13.jpg',
+         alt: 'image',
+         orientation: 'horizontal',
+      },
+      {
+         src: '/images/gallery/img-14.jpg',
+         alt: 'image',
+         orientation: 'horizontal',
+      },
+   ],
+}
+
+export { person, social, newsletter, home, about, blog, work, gallery }
